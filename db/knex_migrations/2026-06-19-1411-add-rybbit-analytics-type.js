f403c9f9cd658b890a1ba8f1248f3a44b7e40204
fgnb666 <156969437+fgnb666@users.noreply.github.com>
2026-07-30 02:37:18 +0000

    clean: remove all old uptime-kuma files, keep only blog source

--- db/knex_migrations/2026-06-19-1411-add-rybbit-analytics-type.js
+++
@@ -1,9 +0,0 @@
-exports.up = function (knex) {
-    // Intentionally left blank - no operation
-    return Promise.resolve();
-};
-
-exports.down = async function (knex) {
-    // Intentionally left blank - no operation
-    return Promise.resolve();
-};
--- scripts/noop.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-// Build placeholder - dist/ is pre-built and committed to the repository
-process.exit(0);
